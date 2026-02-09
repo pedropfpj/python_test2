06f91df176c9ae11c31dcb95dfac81062b354d10
Pedro Freire <pedropfpj@gmail.com>
2026-02-09 11:02:31 -0300

    Adicionar constantes ao data.py

---
+++ data.py
@@ -0,0 +1,16 @@
+# URL do servidor Urban Routes
+URBAN_ROUTES_URL = ''
+
+# Endereços para teste
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+
+# Dados de contato
+PHONE_NUMBER = '+1 123 123 12 12'
+
+# Dados do cartão
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+
+# Mensagem para o motorista
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
