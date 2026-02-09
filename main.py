1dde984aebd3895cc042a3a9523cedfea54e54a7
Pedro Freire <pedropfpj@gmail.com>
2026-02-09 11:32:03 -0300

    Adicionar loop de pedido de sorvete na função test_order_2_ince_creams

--- main.py
+++ main.py
@@ -44,7 +44,10 @@ class TestUrbanRoutes:
     def test_order_2_ice_creams(self):
         # Adicionar em S8
         print("Função criada para pedir 2 sorvetes")
-        pass
+
+        for i in range(2):
+            # Adicionar em S8
+            pass
 
     def test_car_search_model_appears(self):
         # Adicionar em S8
